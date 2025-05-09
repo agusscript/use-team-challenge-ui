6c1a04b4df7238c8692c8e6fbbdb33c01116fa57
agusscript <agus.sanchez.240@gmail.com>
2025-05-09 15:09:57 -0300

    Add main app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import { useState } from 'react'
+import Board from "./components/Board"
+import { DndProvider } from "react-dnd"
+import { HTML5Backend } from "react-dnd-html5-backend"
+import { BoardProvider } from "./context/BoardContext"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <DndProvider backend={HTML5Backend}>
+      <BoardProvider>
+        <div className="flex flex-col h-screen bg-gray-100">
+          <header className="bg-cyan-950 text-white p-4 shadow-md">
+            <div className="container mx-auto">
+              <h1 className="text-xl font-bold">Use Team challenge - Collaborative Board</h1>
+            </div>
+          </header>
+          <div className="flex-1 overflow-hidden">
+            <div className="flex h-full">
+              <main className="flex-1 overflow-auto p-4">
+                <Board />
+              </main>
+            </div>
+          </div>
+        </div>
+      </BoardProvider>
+    </DndProvider>
   )
 }
 
